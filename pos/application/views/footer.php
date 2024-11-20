a2680e70560e9caf6e079e2eab76b56765144b60
riyadhm0bin <mobinul.riyadh@polygontech.xyz>
2024-11-20 17:18:48 +0600

    footer update

--- pos/application/views/footer.php
+++ pos/application/views/footer.php
@@ -1,8 +1,15 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-      <b><span style="color: white;">সকল কারিগরী সহযোগিতায়ঃ  <a href="http://www.elitedesign.com.bd/" target="_blank" title="01732667364"><span style="color: white;">এলিট ডিজাইন</span></a></span></b> 
+        <b>
+            <span style="color: white;">সকল কারিগরী সহযোগিতায়ঃ
+                <a href="https://heaventechit.com" target="_blank">
+                    <span style="color: white;">Heaven Tech-IT</span>
+                </a>
+            </span>
+            <span style="color: white;">, 01521793979</span>
+        </b>
+        <p>সফটওয়্যারের সকল কারিগরী সহযোগিতায় <a style="text-decoration: underline;" href="https://heaventechit.com" class="thembo">Heaven Tech-IT</a></p>
     </div>
-    <strong>সফটওয়্যার ভার্শনঃ ৮.৬</strong>
-  </footer>
-  <!-- Control Sidebar -->
-  
+    <strong>সফটওয়্যার ভার্শনঃ ৯.০</strong>
+</footer>
+<!-- Control Sidebar -->
