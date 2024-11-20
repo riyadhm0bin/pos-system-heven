dc6afdf027d8afd181eb292b44363fc280b567c9
riyadhm0bin <mobinul.riyadh@polygontech.xyz>
2024-11-20 17:22:21 +0600

    footer update

--- pos/application/views/footer.php
+++ pos/application/views/footer.php
@@ -2,13 +2,13 @@
     <div class="pull-right hidden-xs">
         <b>
             <span style="color: white;">সকল কারিগরী সহযোগিতায়ঃ
-                <a href="https://heaventechit.com" target="_blank">
+                <a style="text-decoration: underline;" href="https://heaventechit.com" target="_blank">
                     <span style="color: white;">Heaven Tech-IT</span>
                 </a>
             </span>
             <span style="color: white;">, 01521793979</span>
         </b>
-        <p>সফটওয়্যারের সকল কারিগরী সহযোগিতায় <a style="text-decoration: underline;" href="https://heaventechit.com" class="thembo">Heaven Tech-IT</a></p>
+<!--        <p>সফটওয়্যারের সকল কারিগরী সহযোগিতায় <a style="text-decoration: underline;" href="https://heaventechit.com" class="thembo">Heaven Tech-IT</a></p>-->
     </div>
     <strong>সফটওয়্যার ভার্শনঃ ৯.০</strong>
 </footer>
